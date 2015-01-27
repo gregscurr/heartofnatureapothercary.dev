b2015619ae2c6df5295b5206e7741919f44db206
gregscurr <rothy_@hotmail.com>
2015-01-27 12:30:17 +1100

    update .gitignore and remove master branch

--- components/js/script.js
+++ components/js/script.js
@@ -1,10 +1,10 @@
 
 
 // import jquery
-var script = document.createElement('script');
-script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
+// var script = document.createElement('script');
+// script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+// script.type = 'text/javascript';
+// document.getElementsByTagName('head')[0].appendChild(script);
 
 // import css3 media queries
 // if(typeof Object.create!=="function"){
